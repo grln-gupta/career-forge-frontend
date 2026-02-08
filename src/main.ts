613bba45716f6aa949e0f66c44db4792e265c4dc
guptagrln <guptagrln.sgec@gmail.com>
2026-02-08 19:29:16 +0530

    build error fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { Buffer } from 'buffer';
 import { AppComponent } from './app/app';
 
-bootstrapApplication(AppComponent
-, appConfig)
+(window as any).global = window;
+(window as any).Buffer = Buffer;
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
